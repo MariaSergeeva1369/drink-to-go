d2a4407e10a17fb8e9a49937833f64629a550634
Мария <glsip.girl1995@gmail.com>
2022-12-23 15:20:47 +0300

    Вносит правки в js

--- source/js/nav-toggle.js
+++ source/js/nav-toggle.js
@@ -1,9 +1,35 @@
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const isEscapeKey = (evt) => evt.key === 'Escape';
 
 navMain.classList.remove('main-nav--nojs');
 
+function closeMenulOnEscape (evt) {
+  if(isEscapeKey(evt)) {
+    evt.preventDefault();
+    navMain.classList.remove('main-nav--opened')
+    navMain.classList.add('main-nav--closed');
+  }
+}
+
+function closeMenu () {
+  navMain.classList.remove('main-nav--opened')
+  navMain.classList.add('main-nav--closed');
+
+  document.removeEventListener('keydown', closeMenulOnEscape);
+}
+
+function openMenu () {
+  navMain.classList.remove('main-nav--closed')
+  navMain.classList.add('main-nav--opened');
+
+  document.addEventListener('keydown', closeMenulOnEscape);
+}
+
 navToggle.addEventListener('click', () => {
-  navMain.classList.toggle('main-nav--closed');
-  navMain.classList.toggle('main-nav--opened');
+  if (navMain.classList.contains('main-nav--closed')) {
+    return openMenu();
+  } else {
+    return closeMenu();
+  }
 });
